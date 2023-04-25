6c71e2b46c8410537a5e3947d1e211a259241d90
Sergey <smtishkar@mail.ru>
2023-04-25 19:20:36 +0300

    2. description, count func, zero in the text, random letter

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from aiogram import Bot, Dispatcher, types, executor
+import string
+import random
 
 bot = Bot('5868878555:AAEetjhGk43IZ2LasbIa9vKbzTNCQ92slrs')
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async def echo_upper (message: types.Message):
-    if message.text.count(' ')>=1:
-        await message.answer(text = message.text.upper())
+count = 0
 
+@dp.message_handler(commands=['description'])
+async def desc_command (message: types.message):
+    await message.answer('Данные бот отправляет рандомные символы')
+    await message.delete()
+
+
+@dp.message_handler(commands=['count'])
+async def check_counts(message: types.Message):
+    global count
+    await message.answer(f'COUNT: {count}')
+    count+=1
+
+
+@dp.message_handler()
+async def check_zero (message: types.Message):
+    if "0" in message.text:
+        await message.reply("YES")
+    else:
+        await message.reply("NO")
+
+
+@dp.message_handler()
+async def send_random_letter (message: types.Message):
+    await message.reply(random.choice(string.ascii_letters))   #рандомная буква
 
 if __name__ == '__main__':
     executor.start_polling(dp)
